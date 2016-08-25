e149e8e75141578c5748fa885c5e5ffaba65f8cb
Brian Frederick <briandfrederick@gmail.com>
2016-08-25 14:16:50 -0500

    Add project files.

---
+++ Library List/CardCatalog.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+
+namespace Library_List
+{
+    public class CardCatalog
+    {
+        private string _filename { get; set; }
+        private static List<Book> libraryList = new List<Book>();
+
+        public static void Save(string filename)
+        {
+            var serializer = new XmlSerializer(typeof(List<Book>));
+
+            using (var writer = new StreamWriter
+                ("C:\\Users\\brian\\OneDrive\\Documents\\Visual Studio 2015\\Projects\\Coding Temple Homework\\" + filename + ".XML"))
+            {
+                serializer.Serialize(writer, libraryList);
+            }
+        }
+
+        public static void RetrieveList(string filename)
+        {
+
+            // Create an instance of the XmlSerializer specifying type.
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Book>));
+
+            // Create a TextReader to read the file and designate destination. 
+            FileStream fs = new FileStream("C:\\Users\\brian\\OneDrive\\Documents\\Visual Studio 2015\\Projects\\Coding Temple Homework\\" + filename + ".XML", FileMode.OpenOrCreate);
+            TextReader reader = new StreamReader(fs);
+
+            // Use the Deserialize method to restore the object's state.
+            libraryList = (List<Book>)serializer.Deserialize(reader);
+        }
+
+        public static void listBooks()
+        {
+            foreach (Book item in libraryList)
+            {
+                Console.WriteLine(item.Title + " by " + item.Author);
+            }
+        }
+        public static void addBook(Book bookToAdd)
+        {
+            libraryList.Add(bookToAdd);
+        }
+
+    }
+}
---
+++ Library List/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int choice;
+
+            Console.WriteLine("Hello!");
+            
+            //menu
+            do
+            {
+                Console.WriteLine("\n Press 1 to Add a Book" +
+                                  "\n Press 2 to List All Books" +
+                                  "\n Press 3 to Save to File" +
+                                  "\n Press 4 to Retrieve" +
+                                  "\n Press 5 to Exit" +
+                                  "\n");
+
+                bool number = Int32.TryParse(Console.ReadLine(), out choice);
+
+                if (choice == 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Book Name:");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Author:");
+                    string author = Console.ReadLine();
+                    Book bookToAdd = new Book(name, author);
+                    CardCatalog.addBook(bookToAdd);
+                    Console.Clear();
+                    Console.WriteLine("Adding " + name + " by " + author + ". What's next?");
+                }
+
+                else if (choice == 2)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Your list includes:");
+                    CardCatalog.listBooks();
+                    Console.WriteLine("What's next?");
+                }
+
+                else if (choice == 3)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Provide a file name for your book list.");
+                    string filename = Console.ReadLine();
+                    CardCatalog.Save(filename);
+                    Console.Clear();
+                    Console.WriteLine("File Saved to " + filename + ". Press Enter for Menu.");
+                }
+
+                else if (choice == 4)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Provide a file name for retrieval.");
+                    string filename = Console.ReadLine();
+                    CardCatalog.RetrieveList(filename);
+                    Console.Clear();
+                    Console.WriteLine("List retrieved. What's next?");
+                 }
+
+                else if (choice == 5)
+                {
+                    Console.WriteLine("\nThanks for coming to the library. So long.");
+                    System.Threading.Thread.Sleep(2500);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Valid choices are numbers 1 - 5. Try again.");
+                }
+
+            } while (choice != 5);
+        }
+    }
+}
